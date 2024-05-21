1529600600b65f8db5efe9a8f32b1d5bda462167
RohamIzadidoost <izadidoostroham@gmail.com>
2024-05-21 15:14:50 +0330

    ahoo

---
+++ git_push.cpp
@@ -0,0 +1,23 @@
+//Aho 
+void AddTrie(){
+    int v=0;
+    for (char ch:S){
+        if (!C[v][ch-'a']) C[v][ch-'a']=++ts;
+        v=C[v][ch-'a'];
+    }
+    ted[v]++;
+}
+void BuildAho(){
+    for (int i=0; i<SGM; i++) if (C[0][i]) Q[R++]=C[0][i];
+    while (L<R){
+        int v=Q[L++];
+        ted[v]++;
+        for (int i=0; i<SGM; i++){
+            if (!C[v][i]) C[v][i]=C[F[v]][i];
+            else{
+                F[C[v][i]]=C[F[v]][i];
+                Q[R++]=C[v][i];
+            }
+        }
+    }
+}//
