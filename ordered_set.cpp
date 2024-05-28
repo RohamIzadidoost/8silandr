2e45ffd404607460266308140194435fea57308d
RohamIzadidoost <109338126+RohamIzadidoost@users.noreply.github.com>
2024-05-28 10:04:02 +0330

    Create ordered_set.cpp

---
+++ ordered_set.cpp
@@ -0,0 +1,7 @@
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T , null_type, less<T> , rb_tree_tag , tree_order_statistics_node_update>;
+order_of_key
+find_by_order 
+
