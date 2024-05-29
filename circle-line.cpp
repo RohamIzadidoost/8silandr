1b3db87e4f093c94c1519d204b1ee57985403cfb
Alireza Doostimehr <56887574+alirezadoostimehr@users.noreply.github.com>
2024-05-29 20:12:30 +0330

    Create circle-line.cpp

---
+++ circle-line.cpp
@@ -0,0 +1,11 @@
+typedef pair<point,point> ppp;
+const ld INF = 1e18;
+const ld eps = 1e-15;
+ppp line_circle_intersection(point p1,point p2,point o,ld r){
+  point q = dot(o-p1,p2-p1)/dist(p1,p2)*(p2-p1) + p1;
+  ld d = r*r - dist(o,q);
+  if(d<eps && d>-eps) return ppp(q, point(INF,INF));
+  if(d<0) return ppp(point(INF,INF), point(INF,INF));
+  point dif = sqrt(d/dist(p1,p2))*(p1-p2);
+  return ppp(q-dif,q+dif);
+}
